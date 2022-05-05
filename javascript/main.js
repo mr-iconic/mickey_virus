52b14b44efa9b95ab3f2f595adb1c6d5d0497d4c
mr-iconic <90669066+mr-iconic@users.noreply.github.com>
2022-05-06 00:46:56 +0530

    commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,27 @@
 var hem = document.getElementById("hemb");
 var x = document.getElementById("nav_bar");
-const el=document.getElementById("hemb");
+const el = document.getElementById("hemb");
 hem.addEventListener("click", function myfuntion1() {
    if (x.style.display === "block") {
       x.style.display = "none";
-      el.className="hemburger";
+      el.className = "hemburger";
    }
    else {
       x.style.display = "block";
-      el.className="hemburgerclose";
+      el.className = "hemburgerclose";
    }
 })
 var swt = document.getElementById("theme");
-swt.addEventListener("click",function function2(){
-   if(swt.value === "light"){
-      swt.value="dark";
-      swt.className="darkmode"
-      var right=document.getElementById("switchlight").style.backgroundColor = "#1d858b";
-      var right=document.getElementById("switchlight").style.color="#6d1c06";
-   }else{
-      swt.className="lightmode"
-      swt.value="light";
-      var right=document.getElementById("switchlight").style.backgroundColor = "black"
-      var right=document.getElementById("switchlight").style.color="white";
+swt.addEventListener("click", function function2() {
+   if (swt.value === "light") {
+      swt.value = "dark";
+      swt.className = "darkmode"
+      var right = document.getElementById("switchlight").style.backgroundColor = "#1d858b";
+      var right = document.getElementById("switchlight").style.color = "#6d1c06";
+   } else {
+      swt.className = "lightmode"
+      swt.value = "light";
+      var right = document.getElementById("switchlight").style.backgroundColor = "black"
+      var right = document.getElementById("switchlight").style.color = "white";
    }
 })
