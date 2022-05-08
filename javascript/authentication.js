73a2b42dca3710ad413817cf5460250c9c094f0c
mr-iconic <90669066+mr-iconic@users.noreply.github.com>
2022-05-08 12:20:17 +0530

    commit

---
+++ javascript/authentication.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAIPogACF8zyrPnI8zePJ3HrbJzCPvAxE0",
+    authDomain: "mickey-virus123.firebaseapp.com",
+    projectId: "mickey-virus123",
+    storageBucket: "mickey-virus123.appspot.com",
+    messagingSenderId: "268910520850",
+    appId: "1:268910520850:web:4bd902f375663a9b55c6cb",
+    measurementId: "G-2XZQTPFMTN"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
---
+++ javascript/login.js
@@ -0,0 +1,52 @@
+
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
+    import { getAuth , GoogleAuthProvider ,signInWithRedirect ,getRedirectResult,signOut} from "https://www.gstatic.com/firebasejs/9.8.0/firebase-auth.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyDNUNX5-EFK_fmchxG1XQ2bytimDqk8nB0",
+      authDomain: "my-first-project-542d1.firebaseapp.com",
+      projectId: "my-first-project-542d1",
+      storageBucket: "my-first-project-542d1.appspot.com",
+      messagingSenderId: "178131652235",
+      appId: "1:178131652235:web:eb0c971eeca63f779f4e8e"
+    };
+  
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider(app); 
+
+        var x=document.getElementById("gsignin");
+        x.addEventListener('click',(e)=>{
+
+            signInWithRedirect(auth, provider);
+            getRedirectResult(auth)
+        .then((result) => {
+    
+}).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+});
+})
+
+var y=document.getElementById("signout");
+y.addEventListener('click' ,(e) =>{
+    signOut(auth).then(() => {
+        // Sign-out successful.
+    }).catch((error) => {
+          console.log("signout");
+        // An error happened.
+      });
+})
+
+
+   
